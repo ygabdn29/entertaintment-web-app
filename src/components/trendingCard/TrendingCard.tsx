235e189c54472ab10155cabe2d2c6b812e9999ea
Yoga <yogabudiman07@gmail.com>
2024-03-21 11:31:46 +0700

    feat: adding handlenewbookmark to trendingcard

--- src/components/trendingCard/TrendingCard.tsx
+++ src/components/trendingCard/TrendingCard.tsx
@@ -1,3 +1,5 @@
+import { useShows } from "../../utilities/UseShows";
+
 interface Type {
   small?: string;
   medium?: string;
@@ -23,6 +25,8 @@ interface ShowDetailProps {
 }
 
 function TrendingCard({ show }: ShowDetailProps) {
+  const { handleNewBookmark } = useShows();
+
   return (
     <div className="relative">
       <div className="relative">
@@ -67,9 +71,21 @@ function TrendingCard({ show }: ShowDetailProps) {
         <p className="text-white font-medium text-lg"> {show.title}</p>
       </div>
 
-      <div className="grid place-content-center w-8 h-8 absolute top-4 right-6 bg-dark-blue rounded-full">
-        <img src="src\assets\icon-bookmark-empty.svg" alt="" className="" />
-      </div>
+      <button
+        className={`grid place-content-center w-8 h-8 absolute top-4 right-6 bg-dark-blue text-white ${
+          show.isBookmarked ? "fill-white" : "fill-none"
+        } rounded-full transition-all duration-300 hover:bg-white hover:text-darker-blue`}
+        onClick={() => handleNewBookmark(show)}
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" className="w-3 h-[14px] ">
+          <path
+            d="m10.518.75.399 12.214-5.084-4.24-4.535 4.426L.75 1.036l9.768-.285Z"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            fill="currentFill"
+          />
+        </svg>
+      </button>
     </div>
   );
 }
